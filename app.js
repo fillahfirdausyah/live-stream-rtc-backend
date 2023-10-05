87b88be1cf1f077ff31414cbb2e2813effa45fd4
fillah firdausyah <fillah.akbar@viapulsa.id>
2023-10-05 09:34:31 +0700

    fix: change port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const socketIo = require('socket.io');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new socketIo.Server(server, {
   cors: {
@@ -50,6 +51,6 @@ const onConnection = (socket) => {
 
 io.on('connection', onConnection);
 
-server.listen(5000, () => {
-  console.log('listening on *:5000');
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
